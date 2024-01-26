d32369857947a451c04774ce43295b3cd0b9f369
Fabian Roy <111523461+fabianroy@users.noreply.github.com>
2024-01-26 15:57:02 +0600

    Create script.js

---
+++ Module 16/Video 04/script.js
@@ -0,0 +1,14 @@
+var name = "FAB IAN";
+var address = "Dhaka, Bangladesh";
+
+console.log(name);
+
+var MujtabirisMaiyakhor = true; 
+var MahirPremKorena = false;
+var NasifisHauwarNati = true;
+
+console.log(MujtabirisMaiyakhor);
+console.log(MahirPremKorena);
+console.log(NasifisHauwarNati);
+
+console.log(typeof MujtabirisMaiyakhor);
