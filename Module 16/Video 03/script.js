4ce1fbf979af4e0185ce23ad34ede109c903c334
Fabian Roy <111523461+fabianroy@users.noreply.github.com>
2024-01-26 15:12:33 +0600

    Create script.js

---
+++ Module 16/Video 03/script.js
@@ -0,0 +1,17 @@
+/*  ***********************************************
+
+    * Variable ==> vary + able
+    * 5 things to ddcalre a variable
+
+*/
+
+var weight = 38;
+var price = 38;
+var year = 2004;
+var money = 10000;
+money = 20000;
+
+var age;
+age = 20;
+
+console.log(money);
