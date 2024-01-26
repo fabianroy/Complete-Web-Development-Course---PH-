1d6a684573e9e812cd6f62de2806e3b6976021f8
Fabian Roy <111523461+fabianroy@users.noreply.github.com>
2024-01-26 14:55:19 +0600

    Create script.js

---
+++ Module 16/Video 02/script.js
@@ -0,0 +1,19 @@
+/*  ***********************************************
+
+                MATH FOR JS 
+
+    ***********************************************
+
+    1. Number : +ve, -ve, 0 [50, -9, 0]
+    2. integer : 2, 88, [odd, even] : 2, 4, 897
+    3. float : 2.5, 3.14, 0.5
+    4. Operation: +, -, *, /, %, ++, --
+    5. More Operation: %, [ ]
+    6. Percentage: 5%, 50 taka er 10% koto?
+    7. sum: 14, 18, 16
+    8. Average: 14, 18, 16
+    9. Max / Min: 14, 18, 16
+    10. Series: 
+    11. Area: Triangle, Rectangle, Circle
+    12. 
+*/
