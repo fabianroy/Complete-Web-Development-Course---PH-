c8d10b61cb9052e28934e7c2894f1b0e3c6d3719
Fabian Roy <111523461+fabianroy@users.noreply.github.com>
2024-01-26 16:52:22 +0600

    Create script.js

---
+++ Module 16/Video 06/script.js
@@ -0,0 +1,7 @@
+var num = 10;
+var price = "10";
+var orangePrice = 20;
+var applePrice = parseInt('30');
+console.log(typeof num);
+console.log(typeof price);
+console.log(orangePrice + applePrice);
