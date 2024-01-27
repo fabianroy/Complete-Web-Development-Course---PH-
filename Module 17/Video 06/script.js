878e49e623d8aa81e7920899866e3477c529bf3d
Fabian Roy <111523461+fabianroy@users.noreply.github.com>
2024-01-27 23:48:29 +0600

    Create script.js

---
+++ Module 17/Video 06/script.js
@@ -0,0 +1,15 @@
+var money = 50;
+
+if(money >= 500){
+  console.log("Boroloks");
+} else {
+  if(money >= 300){
+    console.log("GoribBoroloks");
+  } else {
+    if(money >= 100){
+      console.log("Fokinni");
+    } else {
+      console.log("Mara kha ja vaag");
+    }
+  }
+}
