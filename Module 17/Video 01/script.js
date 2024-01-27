142d1aeae259ce6e5f473e4aa814092ca20ad07f
Fabian Roy <111523461+fabianroy@users.noreply.github.com>
2024-01-27 20:45:04 +0600

    Update script.js

--- Module 17/Video 01/script.js
+++ Module 17/Video 01/script.js
@@ -1,1 +1,17 @@
+// Comparison
+/* 
+  
+  1. Bigger >
+  2. Less <
+  3. Equal ==
+  4. Not Equal !=
+  5. Greater than or equal >=
+  6. Less than or equal <=
 
+  -----------------------------------
+
+  7. AND &&
+  8. OR ||
+  9. NOT !
+  
+*/
