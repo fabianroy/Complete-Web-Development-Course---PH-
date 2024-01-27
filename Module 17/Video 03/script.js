9ef494e1ea790c2b5b77ba06a47f3c3f62804e11
Fabian Roy <111523461+fabianroy@users.noreply.github.com>
2024-01-27 22:36:02 +0600

    Create script.js

---
+++ Module 17/Video 03/script.js
@@ -0,0 +1,12 @@
+// If you buy food for me, I will come. 
+
+if(3<10){
+  console.log("I have a smaller value");
+} 
+
+var biryani = 220;
+if(biryani<=200) {
+  console.log("Mama give me a packet of biryani")
+}else {
+  console.log("Mama give me some toast and a cup of tea.");
+}
