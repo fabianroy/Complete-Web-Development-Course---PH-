1f29b20e17842feb898dffbd4fba9a4943dc137d
Fabian Roy <111523461+fabianroy@users.noreply.github.com>
2024-01-27 21:13:13 +0600

    Create script.js

---
+++ Module 17/Video 02/script.js
@@ -0,0 +1,29 @@
+console.log(5>10);
+console.log(10>5);
+console.log(5==10);
+console.log(10==10);
+console.log(5!=10);
+console.log(5>=10);
+console.log(5<=10);
+console.log(10>=10);
+console.log(10<=10);
+console.log(100<=10);
+console.log(100===100);
+
+// -------------------------
+
+var peyaraPrice = 60;
+var applePrice = 140;
+console.log(peyaraPrice > applePrice);
+console.log(peyaraPrice < applePrice);
+console.log(peyaraPrice == applePrice);
+
+// --------------------------
+
+console.log(10 == "10");
+console.log(10 === "10");
+
+// --------------------------
+
+console.log(10 != "10");
+console.log(10!=="10");
