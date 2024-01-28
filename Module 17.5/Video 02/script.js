34e1315b9b8e432991e1f4583ae37be127ed0a01
Fabian Roy <111523461+fabianroy@users.noreply.github.com>
2024-01-29 01:13:11 +0600

    Create script.js

---
+++ Module 17.5/Video 02/script.js
@@ -0,0 +1,11 @@
+const numberA = [12, 22, 33, 57];
+console.log(numberA.length);
+
+const numberB = [12, 22, 33, 57, 68, 75, 88, 98];
+console.log(numberB[2]);
+
+let abc = 45;
+abc = 99; 
+
+numberB[1] = 999;
+console.log(numberB);
