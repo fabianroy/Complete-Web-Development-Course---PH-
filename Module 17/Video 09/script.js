df789449869cba67c31718cbb6474e40620376e5
Fabian Roy <111523461+fabianroy@users.noreply.github.com>
2024-01-28 20:53:32 +0600

    Create script.js

---
+++ Module 17/Video 09/script.js
@@ -0,0 +1,13 @@
+const isLeader = true;
+if(isLeader === true){
+  console.log('Leader ashchee! Chair charo.')
+} else {
+  console.log('Vitore dhukte dibina.')
+}
+
+const isPassed = true;
+if(!isPassed){
+  console.log("Toke biya diya dimu");
+} else {
+  console.log("Porashuna chalay jao");
+}
