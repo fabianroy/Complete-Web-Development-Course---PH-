e326a9a64327061d3d727f972e69f310a6aab462
Fabian Roy <111523461+fabianroy@users.noreply.github.com>
2024-01-30 11:52:18 +0600

    Update script.js

--- Module 18/Video 01 /script.js
+++ Module 18/Video 01 /script.js
@@ -1,1 +1,27 @@
+/* 
 
+const = the value of const won't change.
+let = the value can be changed. 
+var = we should not use it. 
+
+*/
+
+const numbers = [1, 2, 3, 4, 5];
+const amount = 100;
+let name = "Lenovo";
+var age = 20;
+
+// For Loop
+
+for (const num of numbers){
+  console.log(num);
+}
+
+for (let i = 0; i < 5; i++){
+  console.log('Hello World');
+}
+
+const fruits = ['Apple', 'Jackfruits', 'Oranges', 'Mangoes'];
+for(let fruit of fruits){
+  console.log("I want to eat " + fruit);
+}
