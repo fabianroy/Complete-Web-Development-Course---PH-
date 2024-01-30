21f46bb725a4e39edacebebb9ea73c8523a8fcde
Fabian Roy <111523461+fabianroy@users.noreply.github.com>
2024-01-30 19:31:03 +0600

    Create script.js

---
+++ Module 18/Video 03/script.js
@@ -0,0 +1,26 @@
+// While Loop 
+
+let num = 1;
+while (num <= 10) {
+  console.log(num);
+  num++;
+}
+
+let numA = 1;
+let sum = 0;
+while (numA <= 10){
+  sum = sum + numA
+  console.log(sum);
+  numA++;
+}
+
+// Even Number 
+
+let numB = 1;
+while (numB <= 10){
+  console.log(numB);
+  if (numB % 2 == 0){
+    console.log('Even =', numB);
+  }
+  numB++;
+}
