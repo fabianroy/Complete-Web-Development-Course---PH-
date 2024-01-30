71caa3eeb20c5c4f344331fe38d2cf1652690c12
Fabian Roy <111523461+fabianroy@users.noreply.github.com>
2024-01-30 20:20:21 +0600

    Update script.js

--- Module 18/Video 05/script.js
+++ Module 18/Video 05/script.js
@@ -1,1 +1,13 @@
+let sum = 0;
+for (i = 11; i <=20; i++){
+  sum = sum + i;
+  console.log(i);
+}
 
+console.log('Sum of number 11 to 20 is = ', sum);
+
+// Decremental For Loop
+
+for (let x = 100; x >= 1; x--){
+  console.log(x);
+}
