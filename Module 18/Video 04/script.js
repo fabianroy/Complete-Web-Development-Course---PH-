de88a17dcf66463aef3028ad41733fe5986d20e5
Fabian Roy <111523461+fabianroy@users.noreply.github.com>
2024-01-30 20:05:49 +0600

    Update script.js

--- Module 18/Video 04/script.js
+++ Module 18/Video 04/script.js
@@ -1,1 +1,19 @@
+for (let x = 1; x <= 5; x++){
+  console.log(x);
+}
 
+// ODD
+
+for (let i = 0; i <= 100; i++) {
+  if (i % 2 !== 0) {
+    console.log(i);
+  }
+}
+
+// EVEN
+
+for (let o = 0; o < 100; o++) {
+  if (o % 2 == 0) {
+    console.log(o);
+  }
+}
