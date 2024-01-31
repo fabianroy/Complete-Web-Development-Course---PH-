27347d231f69b3c94607efdb3af0461559cca9d4
Fabian Roy <111523461+fabianroy@users.noreply.github.com>
2024-02-01 02:09:52 +0600

    Create script.js

---
+++ Module 19/Video 05/script.js
@@ -0,0 +1,28 @@
+let age = 21;
+let school = "SJS";
+let ispassesed = true;
+let isDev;
+let subjects = ['Bangla', 'English', 'Math', 'Science', 'History', 'Geography'];
+console.log(isDev);
+
+// Object
+let bottle = {
+  a : 5,
+  b : 10,
+  c : 15,
+}
+console.log(bottle);
+console.log(bottle.a);
+
+let student = {
+  name : 'Fabian',
+  age : 21,
+  school : 'SJS',
+  subjects : ['Bangla', 'English', 'Math'],
+  exam : {
+    name: 'Final Exam',
+    result: 3.67
+  }
+}
+
+console.log(student);
