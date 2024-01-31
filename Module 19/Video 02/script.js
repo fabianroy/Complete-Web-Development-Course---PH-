6ea4b4b0eb46693ef67329857b9b4e694464987d
Fabian Roy <111523461+fabianroy@users.noreply.github.com>
2024-02-01 00:58:48 +0600

    Update script.js

--- Module 19/Video 02/script.js
+++ Module 19/Video 02/script.js
@@ -1,1 +1,27 @@
+let subject = "BUS 101";
+let course = "bus 101";
+if (subject === course) {
+  console.log("Fundamentals of Business Studies");
+} else {
+  console.log("Subject and Course do not match");
+}
 
+let a = "JUST A STRING";
+console.log(a.toLowerCase());
+
+let b = "just a string";
+console.log(b.toUpperCase());
+
+if (a.toLocaleLowerCase() === b.toLocaleLowerCase()) {
+  console.log("Same");
+} else {
+  console.log("Different");
+}
+
+let drink = "Water";
+let liquid = " Water    ";
+if(drink.trim() == liquid.trim()){
+  console.log("Okay");
+} else {
+  console.log("Not Okay");
+}
