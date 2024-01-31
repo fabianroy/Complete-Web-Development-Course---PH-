1251a6b06119b87bf727bd26e9e75e311e2e3947
Fabian Roy <111523461+fabianroy@users.noreply.github.com>
2024-01-31 23:02:32 +0600

    Update script.js

--- Module 19/Video 01/script.js
+++ Module 19/Video 01/script.js
@@ -1,1 +1,19 @@
-M
+let city = "Dhaka";
+let country = 'Bangladesh';
+let area = `Mirpur`;
+let thana = new String("Mirpur");
+console.log(country.length);
+console.log(typeof city);
+console.log(typeof area);
+console.log(typeof thana);
+
+let number = [1, 54, 84, 544, 878, 998];
+console.log(number[0]);
+console.log(area[0]);
+
+let shohor = "DHAKA";
+shohor[0] = "F";
+console.log(shohor); // Cannot change cz it is immutable
+
+// string is immutable --> not changable
+// array is mutab;e --> changable 
