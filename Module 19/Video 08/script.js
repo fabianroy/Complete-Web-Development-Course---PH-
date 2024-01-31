579cc8258a69281bd7126364f476102aa02ef939
Fabian Roy <111523461+fabianroy@users.noreply.github.com>
2024-02-01 03:00:34 +0600

    Update script.js

--- Module 19/Video 08/script.js
+++ Module 19/Video 08/script.js
@@ -1,1 +1,30 @@
+let mobile = {
+  brand : 'apple',
+  price : 155000,
+  color : 'gold',
+  storage : 512,
+  isNew : true
+}
 
+// For of : Array
+// For in : Object
+
+for(let prop in mobile){
+  console.log(prop);
+  console.log(mobile[prop]);
+}
+
+console.log(Object.keys(mobile)); // Array
+
+for(let key of Object.keys(mobile)){
+  console.log(key, ':', mobile[key]);
+}
+
+// ---------------------------------------------------------
+
+let pen = {brand: 'matador', price : 20, color : 'black'}
+let pencil = new Object();
+console.log(pen);
+console.log(pencil);
+let rubber = Object.create({});
+console.log(rubber);
