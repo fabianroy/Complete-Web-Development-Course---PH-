94531d6a474b7be8ddd57b6816a05d8a9c229acc
Fabian Roy <111523461+fabianroy@users.noreply.github.com>
2024-02-01 03:41:57 +0600

    Create task4.js

---
+++ Module 19/Video 09 (Tasks) /task4.js
@@ -0,0 +1,9 @@
+// If a given string has either x, replace x by y. if the given string has X, replace it by Y.
+
+let a = 'x';
+if (a == 'x'){
+  a = 'y';
+} else if (a == 'X'){
+  a = 'Y';
+}
+console.log(a);
