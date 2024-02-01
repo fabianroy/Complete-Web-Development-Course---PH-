8ab420ceb79c3d8f5acb4baffb049b9d75265b33
Fabian Roy <111523461+fabianroy@users.noreply.github.com>
2024-02-01 18:36:46 +0600

    Update script.js

--- Module 19.5/Video 04/script.js
+++ Module 19.5/Video 04/script.js
@@ -2,4 +2,8 @@
 let numbers = ['Tom', 'Tim', 'Tim', 'Tik'];
 console.log(numbers.join(''));
 
-//
+// task 4 reverse words
+let number = ['Tom', 'Tim', 'Tim', 'Tik'];
+for(let i = number.length; i >= 0; i--){
+  console.log(number[i]);
+}
