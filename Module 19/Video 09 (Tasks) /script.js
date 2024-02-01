367ef9a3d490083e5dc8b25bec30b69dabf8160b
Fabian Roy <111523461+fabianroy@users.noreply.github.com>
2024-02-01 23:33:48 +0600

    Create script.js

---
+++ Module 19/Video 09 (Tasks) /script.js
@@ -0,0 +1,15 @@
+// task 3
+let numbers = ['Tom', 'Tim', 'Tim', 'Tik'];
+console.log(numbers.join(''));
+
+// task 4 reverse words
+let number = ['Tom', 'Tim', 'Tim', 'Tik'];
+for(let i = number.length; i >= 0; i--){
+  console.log(number[i]);
+}
+
+// task mySelf
+let num = ['Tom', 'Tim', 'Tim', 'Tik'];
+if (num.includes('Tom')) {
+  console.log("Tom is in the array");
+}
