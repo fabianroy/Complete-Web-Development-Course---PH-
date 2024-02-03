23a5aa7cfc09a54a422e3c262e10f11491ee2643
Fabian Roy <111523461+fabianroy@users.noreply.github.com>
2024-02-03 14:42:04 +0600

    Create script.js

---
+++ Module 20/Video 02/script.js
@@ -0,0 +1,26 @@
+function vatKhao(){
+  console.log("hat dhuye asho.");
+  console.log("tarpor table e asho.");
+  console.log("vat bara ache torkari nao.");
+  console.log("dhire shusthe pet vore khao.")
+  console.log("Nasif er moton gopat gopat kore khaiyo na.");
+  console.log("Naile shiri theke pore hatu venge ghore boshe thakte hobe")
+}
+
+vatKhao();
+
+function square (number){
+  console.log(number);
+  console.log('Value of the number parameter', number);
+  let borgo = number * number;
+  console.log('Borgo of the given numbers is', borgo);
+}
+
+square (4);
+
+function add(num1, num2, num3, num4){
+  let number = (num1 + num2) / (num3 + num4);
+  console.log('Sum of the given numbers is', number);
+}
+
+add(10, 5, 1, 2);
