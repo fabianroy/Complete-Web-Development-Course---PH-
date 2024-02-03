977b939f2e7c93eb27166c5ebe20a670efbd9715
Fabian Roy <111523461+fabianroy@users.noreply.github.com>
2024-02-03 12:30:48 +0600

    Create script.js

---
+++ Module 20/Video 01/script.js
@@ -0,0 +1,8 @@
+let number = 45;
+function fanOffKor() {
+  console.log("Bosha theke uthe dara");
+  console.log("Tarpor switch er kase ja");
+  console.log("Switch tipe off kore de");
+}
+
+fanOffKor();
