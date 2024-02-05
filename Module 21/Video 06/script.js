591056120364c8ff57272a6bbbcd5743400e2ba7
Fabian Roy <111523461+fabianroy@users.noreply.github.com>
2024-02-05 22:01:32 +0600

    Create script.js

---
+++ Module 21/Video 06/script.js
@@ -0,0 +1,30 @@
+// let Math = {
+//   main: function min(num1, num2){
+//     return num2;
+//   }
+// }
+
+// ***************** MATH : Min & Max *****************
+console.log(Math.min(45, -99, 2, -55, 0, -4, 33));  // -99
+console.log(Math.max(45, -99, 2, -55, 0, -4, 33));  // 45
+
+// ***************** MATH : PI *****************
+console.log(Math.PI); // 3.141592653589793
+
+// ***************** MATH : abs *****************
+console.log(Math.abs(10 - 15)); // Gives the absolute value of the number (** GAP **) (ans : 5)
+
+// ***************** MATH : Round *****************
+console.log(Math.round(5.6)); // Rounds the number to the nearest integer (ans : 6)
+
+// ***************** MATH : Floor *****************
+console.log(Math.floor(5.6)); // Rounds the number down to the nearest integer (ans : 5)
+
+// ***************** MATH : Ceil *****************
+console.log(Math.ceil(5.6)); // Rounds the number up to the nearest integer (ans : 6)
+
+// ***************** MATH : Random *****************
+console.log(Math.random()*10);
+console.log(Math.random()*10);
+const result = Math.round(Math.random()*10);
+console.log(result);
