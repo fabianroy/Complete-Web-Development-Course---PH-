cd14f74ca98d572eb1f72223b6d97cb32b152905
Fabian Roy <111523461+fabianroy@users.noreply.github.com>
2024-02-05 23:26:57 +0600

    Update script.js

--- Module 21/Video 07/script.js
+++ Module 21/Video 07/script.js
@@ -1,1 +1,11 @@
+let date = new Date();
+console.log(date);
 
+let newDate = new Date('2024-02-13');
+console.log(newDate);
+console.log(newDate.getMonth());
+console.log(newDate.getDay());
+
+let specificDate = new Date(2091, 0, 26);
+specificDate.setMonth(10);
+console.log(specificDate.toLocaleString('en-GB'));
