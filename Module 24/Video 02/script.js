b2244b1d41155161bb86043182f421fdb465f063
Fabian Roy <111523461+fabianroy@users.noreply.github.com>
2024-02-10 02:28:29 +0600

    Create script.js

---
+++ Module 24/Video 02/script.js
@@ -0,0 +1,12 @@
+console.log('Hello, World!');
+console.log(document);
+
+let liCollection = document.getElementsByTagName('li');
+for (let li of liCollection) {
+    //console.log(li);
+}
+
+let allHeading = document.getElementsByTagName('h1');
+for (let heading of allHeading) {
+    //console.log(heading.innerText);
+}
