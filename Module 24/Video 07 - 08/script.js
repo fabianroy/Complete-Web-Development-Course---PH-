252e726e8d93411ba8e21c03bb214ab40817b97f
Fabian Roy <111523461+fabianroy@users.noreply.github.com>
2024-02-10 21:08:46 +0600

    Create script.js

---
+++ Module 24/Video 07 - 08/script.js
@@ -0,0 +1,45 @@
+const navList = document.getElementById('navbar');
+let li = document.createElement('li');
+li.innerText = 'Shop';
+navList.appendChild(li);
+
+// ----------------------------------------------
+
+// Where to add
+let mainContainer = document.getElementById('main');
+
+// What to add
+let section = document.createElement('section');
+let h1 = document.createElement('h1');
+h1.innerText = 'Products';
+section.appendChild(h1);
+
+let ul = document.createElement('ul');
+let li1 = document.createElement('li');
+li1.innerText = 'Web Hosting';
+let li2 = document.createElement('li');
+li2.innerText = 'Domain Services';
+let li3 = document.createElement('li');
+li3.innerText = 'Cloud Services';
+ul.appendChild(li1);
+ul.appendChild(li2);
+ul.appendChild(li3);
+
+section.appendChild(ul);
+
+mainContainer.appendChild(section);
+
+// Ser innerHTML direclty
+
+let sectionDress = document.createElement('section');
+sectionDress.innerHTML = `
+
+<h1>Dresses</h1>
+<ul>
+  <li>Summer Dress</li>
+  <li>Winter Dress</li>
+  <li>Spring Dress</li>
+</ul>
+
+`
+mainContainer.appendChild(sectionDress);
