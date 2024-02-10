04035cca95048219545aabf2efcb19c759f65359
Fabian Roy <111523461+fabianroy@users.noreply.github.com>
2024-02-10 13:54:25 +0600

    Create script.js

---
+++ Module 24/Video 05/script.js
@@ -0,0 +1,8 @@
+document.getElementById('fruit-sec').style.backgroundColor = 'red';
+document.getElementById('fruit-sec').style.color = 'white';
+document.getElementById('Fruits').style.color = 'white';
+document.getElementById('Fruits').style.fontSize = '30px';
+document.getElementById('Fruits').style.textAlign = 'center';
+document.getElementById('fruit-sec').style.padding = '20px';
+
+document.getElementById('Fruits').innerText = 'Fol';
