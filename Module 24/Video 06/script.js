73e763b359204e4f1d0cd1d7459bb48229278692
Fabian Roy <111523461+fabianroy@users.noreply.github.com>
2024-02-10 16:19:43 +0600

    Update script.js

--- Module 24/Video 06/script.js
+++ Module 24/Video 06/script.js
@@ -1,1 +1,14 @@
+let sections = document.querySelectorAll('section');
+for(let section of sections){
+    section.style.color='red';
+    section.style.backgroundColor='yellow';
+    section.style.padding='20px';
+    section.style.border='1px solid black';
+    section.style.margin='20px';
+    section.style.borderRadius='10px';
+    section.style.boxShadow='0 0 10px black';
+}
 
+let h2 = document.getElementById('hello');
+h2.classList.add('text-center');
+h2.classList.remove('largeText');
