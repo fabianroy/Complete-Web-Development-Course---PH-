350d09041252aeabd9f857edb428d1467da9b61e
Fabian Roy <111523461+fabianroy@users.noreply.github.com>
2024-02-11 14:26:18 +0600

    Create script.js

---
+++ Module 25/Video 02/script.js
@@ -0,0 +1,19 @@
+function ck(){
+    alert("Yo! Tumi amake click korso!!");
+}
+
+function ack(){
+    document.write("Tumi amake abar click korso!");
+}
+
+function bgcolor() {
+  document.body.style.backgroundColor = "yellow";
+}
+
+let bgWhite = document.getElementById("bgWhite");
+bgWhite.onclick = 
+
+function bgcolor2() {
+  document.body.style.backgroundColor = "white";
+  document.querySelector('h1').style.color = "green";
+}
