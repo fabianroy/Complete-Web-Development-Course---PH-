c0c3baea03378a757141dd148000d052c5c48481
Fabian Roy <111523461+fabianroy@users.noreply.github.com>
2024-02-12 12:15:44 +0600

    Update script.js

--- Module 25/Video 07/script.js
+++ Module 25/Video 07/script.js
@@ -1,1 +1,19 @@
+document.getElementById('item-2').addEventListener('click', function(event) {
+    console.log('item  2 clicked');
+    event.stopPropagation();
+})
 
+document.getElementById('list').addEventListener('click', function(event) {
+    console.log('list clicked');
+    event.stopPropagation();
+})
+
+document.getElementById('container').addEventListener('click', function(event) {
+    console.log('container clicked');
+    event.stopPropagation();
+})
+
+document.getElementById('body').addEventListener('click', function(event) {
+    console.log('body clicked');
+    event.stopPropagation();
+})
