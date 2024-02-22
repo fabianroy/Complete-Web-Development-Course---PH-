7d7836c07235dd8797b6d3f62ff5bfb1a232ca45
Fabian Roy <111523461+fabianroy@users.noreply.github.com>
2024-02-22 16:36:56 +0600

    Create script.js

---
+++ Module 30/Video 03/script.js
@@ -0,0 +1,22 @@
+const first = 'Nikolay';
+const last = 'Kostov';
+console.log(first + ' ' + last);
+
+const a = 5;
+const b = 6;
+const result = 'The sum of ' + a + ' and ' + b + ' is ' + (a + b);
+console.log(result);
+
+const math = `The sum of ${a} and ${b} is ${a+b}`;
+console.log(math);
+
+const email = 'Hi john ke \n' + 
+'cena cena lage';
+console.log(email);
+
+// Template String
+
+const challenge = `John Cena 
+cana naki ochena
+mair dimu prochur`;
+console.log(challenge);
