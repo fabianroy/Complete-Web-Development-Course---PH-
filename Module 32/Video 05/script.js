b83cda1d397ee2fc7dbff26407365671321fd2d1
Fabian Roy <111523461+fabianroy@users.noreply.github.com>
2024-02-24 22:12:15 +0600

    Create script.js

---
+++ Module 32/Video 05/script.js
@@ -0,0 +1,14 @@
+print5();
+for(let i=0; i<5; i++) {
+  console.log(i)
+}
+// console.log('Outside', i);
+
+function print5(){
+    console.log(5);
+}
+
+const print10 = function(){
+    console.log(10);
+};
+print10();
