3dc0f21202fb193277fcf083f56fec2f4851ee09
Fabian Roy <111523461+fabianroy@users.noreply.github.com>
2024-02-24 21:59:40 +0600

    Create script.js

---
+++ Module 32/Video 04/script.js
@@ -0,0 +1,32 @@
+let x = 2;
+let y = 2;
+if (x == y) {
+    console.log("x is equal to y");
+} else {
+    console.log("x is not equal to y");
+}
+
+let a = 2;
+let b = '2';
+if (a == b) {
+    console.log("a is equal to b");
+}
+else {
+    console.log("a is not equal to b");
+}
+
+let first = 2;
+let second = '2';
+if (first === second) {
+    console.log("first is equal to second");
+} else {
+    console.log("first is not equal to second");
+}
+
+let p = 2;
+let q = 2;
+if (p === q) {
+    console.log("p is equal to q");
+} else {
+    console.log("p is not equal to q");
+}
