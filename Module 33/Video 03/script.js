64becfa9a2d261b39ec8221c8c9f339e6a0fe2c8
Fabian Roy <111523461+fabianroy@users.noreply.github.com>
2024-02-27 20:51:51 +0600

    Update script.js

--- Module 33/Video 03/script.js
+++ Module 33/Video 03/script.js
@@ -1,3 +1,7 @@
-fetch('https://jsonplaceholder.typicode.com/todos/1')
-    .then(response => console.log(response)) // .json is not similae but close to JSON.parse
-   // .then(json => console.log(json))
+const url = 'https://jsonplaceholder.typicode.com/todos/1';
+
+function loadData(){
+    fetch(url)
+.then(response => response.json())
+.then(json => console.log(json))
+}
